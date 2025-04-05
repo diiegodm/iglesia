c3b01bc3113c2c3a4aa31a5d349a2d2977439023
diiegodm <diegoarevalodm@gmail.com>
2025-04-05 16:51:22 +0200

    creo qy ya

--- iglesia/src/app/navbar/navbar.component.ts
+++ iglesia/src/app/navbar/navbar.component.ts
@@ -3,11 +3,12 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
   menuActivo: boolean = false;
 
   toggleMenu() {
